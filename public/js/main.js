bac919a0097a0a8298e34d87a67b741d768c960d
chris <teihyung@gmail.com>
2024-01-01 22:28:22 -0800

    back to original

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,13 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
+});
+
+
 window.addEventListener('load', function() {
   setTimeout(function() {
     document.querySelector('.Home_Container').classList.add('visible');
@@ -27,14 +37,5 @@ window.addEventListener('load', function() {
     document.querySelector('.used_skill').classList.add('visible');
   }, 400); // The delay before the class is added, in milliseconds
 });
-
-// const toggleBtn = document.querySelector('.navbar__toggleBtn');
-// const menu = document.querySelector('.navbar__menu');
-// const icons = document.querySelector('.navbar__icons');
-//
-// toggleBtn.addEventListener('click', () => {
-//   menu.classList.toggle('active');
-//   icons.classList.toggle('active');
-// });
 
 
