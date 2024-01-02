c1e0ebc2c2949dbdcdcc28b01737894087c33787
chris <teihyung@gmail.com>
2024-01-01 22:25:19 -0800

    tryout

--- public/js/main.js
+++ public/js/main.js
@@ -28,13 +28,13 @@ window.addEventListener('load', function() {
   }, 400); // The delay before the class is added, in milliseconds
 });
 
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  icons.classList.toggle('active');
-});
+// const toggleBtn = document.querySelector('.navbar__toggleBtn');
+// const menu = document.querySelector('.navbar__menu');
+// const icons = document.querySelector('.navbar__icons');
+//
+// toggleBtn.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   icons.classList.toggle('active');
+// });
 
 
